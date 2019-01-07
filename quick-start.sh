a5a238611978b4c089a5d7d4a1b6f90beae8d4b1
Greg Leitheiser <greg@servantscode.org>
2019-01-07 16:40:10 -0600

    common logging

--- quick-start.sh
+++ quick-start.sh
@@ -3,7 +3,7 @@
 SERVICE=$1
 echo "Configuring service $SERVICE"
 
-files="Dockerfile deploy.env docker-compose.yml settings.gradle src/main/resources/log4j2.json"
+files="Dockerfile deploy.env docker-compose.yml settings.gradle"
 
 for file in $files; do 
   echo "Updating $file"
