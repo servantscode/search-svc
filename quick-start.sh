d2c760296958fa457ec4101cff563c1339ba2db8
Greg Leitheiser <greg@servantscode.org>
2019-01-08 10:51:40 -0600

    Added service name to log context

--- quick-start.sh
+++ quick-start.sh
@@ -3,17 +3,19 @@
 SERVICE=$1
 echo "Configuring service $SERVICE"
 
-files="Dockerfile deploy.env docker-compose.yml settings.gradle"
+files="Dockerfile deploy.env docker-compose.yml settings.gradle src/main/java/org/servantscode/hello/rest/ServiceIdentificationFilter.java"
 
 for file in $files; do 
   echo "Updating $file"
   sed -i "s/hello/$SERVICE/g" $file
 done
 
+mv src/main/java/org/servantscode/hello src/main/java/org/servantscode/$SERVICE
+
 SERVICE="$(tr '[:lower:]' '[:upper:]' <<< ${SERVICE:0:1})${SERVICE:1}"
 DATE=`date +%D`
 USER=`whoami`
-printf "$SERVICE service\nCreator: $USER\nCreated: $DATE" >README.md
+printf "Service: $SERVICE\nCreator: $USER\nCreated: $DATE" > README.md
 
 git remote set-url origin git@github.com:servantscode/$SERVICE.git
 git push
